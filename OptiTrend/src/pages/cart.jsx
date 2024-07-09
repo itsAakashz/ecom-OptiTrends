9cb2369cb12906d138af5a70fb7cd714b13445d1
Aakash kumar <76447347+itsAakashz@users.noreply.github.com>
2024-07-10 01:54:38 +0530

    Update cart.jsx

--- OptiTrend/src/pages/cart.jsx
+++ OptiTrend/src/pages/cart.jsx
@@ -40,12 +40,11 @@ const Cart = () => {
   ];
   const subtotal = cartItems.reduce((acc, item) => acc + item.price, 0);
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
+    <div className="min-h-screen p-4">
       <div className="container mx-auto bg-white shadow-md rounded-lg p-6">
         {/* upper container here */}
         <div className="mt-6 flex justify-between items-center">
           <Link to="/">
-            {" "}
             <div className="text-left flex">
               <IoIosArrowRoundBack className="w-10 h-6" />
               Back
@@ -54,43 +53,47 @@ const Cart = () => {
           <h2 className="text-2xl font-semibold text-center flex-grow">Cart</h2>
           <button className="justify-end content-end ml-auto  lg:py-1 px-2 h-7 lg:h-10 text-center border-black rounded-[10px] border-2 border-solid text-black"> Clear All </button>
         </div>
+        
         {/* Product conatiner container here */}
-        <div className="space-y-4">
+        <div className="space-y-5">
           {cartItems.map((item) => (
             // product conatiner Upper side
             <div key={item.id} >
-              <div className="flex items-center justify-between p-1 mt-8">
+              <div className="flex items-center justify-between mt-8">
                   <img
                     src={item.img}
                     alt={item.name}
-                    className="w-28 lg:h-15 object-cover rounded md:w-[220px] md:h-[170]"
+                    className="w-28 lg:h-15 h-[px] lg:mb-16 md:w-[220px] md:h-[170]"
                   />
                   <div className="">
                     <p className="text-gray-600 hidden sm:block">{`Brand: ${item.brand} | Similar product from ${item.brand}`}</p>
-                    <h3 className="text-lg font-medium">{item.name}</h3>
+                    <h3 className="lg:text-lg lg:text-[12px] text-[0.6rem] font-medium">{item.name}</h3>                 
+                    <div className="flex  md:pr-50 ">
+                      <div className="flex-col lg:m-5 m-1">
+                        <p className="text-gray-600 text-[14px] lg:text-[18px] lg:m-2  mt-4">Size</p>
+                        <p className="text-gray-600 text-[13px] lg:text-[18px] lg:m-2 mt-3">{item.size}</p>
+                      </div>
+                      
+                      <div className="flex-col ml-1 md:ml-5 my-5">
+                        <p className="text-gray-600 text-[13px] lg:text-[18px] lg:m-2">Color</p>
+                        <GoDotFill className="h-[24px] w-[24px] m-2" />
+                      </div>
+                    </div>
                   </div>
                   <div className="text-right">
-                    <p className="text-[28px] font-bold">${item.price}</p>
+                    <p className="lg:text-[28px] text-[18px] font-bold">${item.price}</p>
                   </div>
               </div>
               <div>
               </div>
               {/* // product conatiner lower side */}
               <div className="flex items-center justify-between p-1">
-                <button className="w-[220px] px-4 lg:px-2 lg:h-7 text-center border-black rounded border-2 border-solid text-black">
+                
+                <button className="lg:w-[220px] px-1 lg:px-4 lg:p-0 p-1 lg:h-7 text-center border-black rounded border-[1px] border-solid text-black">
                   Remove Item
                 </button>
-                <div className="flex pr-0 md:pr-50 ">
-                  <div className="flex-col m-5">
-                    <p className="text-gray-600 m-2">Size</p>
-                    <p className="text-gray-600 m-2">{item.size}</p>
-                  </div>
-                  <div className="flex-col ml-1 md:ml-5 my-5">
-                    <p className="text-gray-600 text-[16px] m-2">Color</p>
-                    <GoDotFill className="h-[24px] w-[24px] m-2" />
-                  </div>
-                </div>
-                <div className="flex mt-32 md:mt-0 ">
+               
+                <div className="flex mt-25 md:mt-0 ">
                   <Link className="pr-2 h-[40px] w-[40px]"><img src={minus_icon}/></Link>
                   <p className="px-2  text-[18px]">1</p>
                   <Link className="pr-2 h-[40px] w-[40px]"><img src={plus_icon}/></Link>
