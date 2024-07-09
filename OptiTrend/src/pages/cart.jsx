2567ee5ba38ab99d1b2ca014f1343759faa2a2f1
Aakash kumar <76447347+itsAakashz@users.noreply.github.com>
2024-07-10 02:49:21 +0530

    Update cart.jsx

--- OptiTrend/src/pages/cart.jsx
+++ OptiTrend/src/pages/cart.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { IoIosArrowRoundBack } from "react-icons/io";
-import cartImage1 from "../assets/images/glasses/salt-image-1.png"; // replace with actual image paths
-import cartImage2 from "../assets/images/glasses/salt-image-10.png";
-import cartImage3 from "../assets/images/glasses/salt-image-5.png";
 import { GoDotFill } from "react-icons/go";
-import { PiMinusSquareDuotone } from "react-icons/pi";
 import minus_icon from "../assets/minus.png";
 import plus_icon from "../assets/plus.png";
+import cartImage1 from "../assets/images/glasses/salt-image-1.png";
+import cartImage2 from "../assets/images/glasses/salt-image-10.png";
+import cartImage3 from "../assets/images/glasses/salt-image-5.png";
+
 const Cart = () => {
   const cartItems = [
     {
@@ -38,7 +38,33 @@ const Cart = () => {
       color: "black",
     },
   ];
-  const subtotal = cartItems.reduce((acc, item) => acc + item.price, 0);
+
+  const [quantities, setQuantities] = useState(
+    cartItems.reduce((acc, item) => {
+      acc[item.id] = 1; // Initialize all quantities to 1
+      return acc;
+    }, {})
+  );
+
+  const handleIncrease = (id) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [id]: prevQuantities[id] + 1,
+    }));
+  };
+
+  const handleDecrease = (id) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [id]: prevQuantities[id] > 1 ? prevQuantities[id] - 1 : 1,
+    }));
+  };
+
+  const subtotal = cartItems.reduce(
+    (acc, item) => acc + item.price * quantities[item.id],
+    0
+  );
+
   return (
     <div className="min-h-screen p-4">
       <div className="container mx-auto bg-white shadow-md rounded-lg p-6">
@@ -53,11 +79,11 @@ const Cart = () => {
           <h2 className="text-2xl font-semibold text-center flex-grow">Cart</h2>
           <button className="justify-end content-end ml-auto  lg:py-1 px-2 h-7 lg:h-10 text-center border-black rounded-[10px] border-2 border-solid text-black"> Clear All </button>
         </div>
-        
-        {/* Product conatiner container here */}
+
+        {/* Product container here */}
         <div className="space-y-5">
           {cartItems.map((item) => (
-            // product conatiner Upper side
+            // product container Upper side
             <div key={item.id} >
               <div className="flex items-center justify-between mt-8">
                   <img
@@ -67,13 +93,13 @@ const Cart = () => {
                   />
                   <div className="">
                     <p className="text-gray-600 hidden sm:block">{`Brand: ${item.brand} | Similar product from ${item.brand}`}</p>
-                    <h3 className="lg:text-lg lg:text-[12px] text-[0.6rem] font-medium">{item.name}</h3>                 
+                    <h3 className="lg:text-lg lg:text-[18px] text-[0.6rem] font-medium">{item.name}</h3>                 
                     <div className="flex  md:pr-50 ">
                       <div className="flex-col lg:m-5 m-1">
                         <p className="text-gray-600 text-[14px] lg:text-[18px] lg:m-2  mt-4">Size</p>
                         <p className="text-gray-600 text-[13px] lg:text-[18px] lg:m-2 mt-3">{item.size}</p>
                       </div>
-                      
+
                       <div className="flex-col ml-1 md:ml-5 my-5">
                         <p className="text-gray-600 text-[13px] lg:text-[18px] lg:m-2">Color</p>
                         <GoDotFill className="h-[24px] w-[24px] m-2" />
@@ -86,17 +112,17 @@ const Cart = () => {
               </div>
               <div>
               </div>
-              {/* // product conatiner lower side */}
+              {/* // product container lower side */}
               <div className="flex items-center justify-between p-1">
-                
+
                 <button className="lg:w-[220px] px-1 lg:px-4 lg:p-0 p-1 lg:h-7 text-center border-black rounded border-[1px] border-solid text-black">
                   Remove Item
                 </button>
-               
+
                 <div className="flex mt-25 md:mt-0 ">
-                  <Link className="pr-2 h-[40px] w-[40px]"><img src={minus_icon}/></Link>
-                  <p className="px-2  text-[18px]">1</p>
-                  <Link className="pr-2 h-[40px] w-[40px]"><img src={plus_icon}/></Link>
+                  <Link className="pr-2 h-[40px] w-[40px]" onClick={() => handleDecrease(item.id)}><img src={minus_icon}/></Link>
+                  <p className="px-2  text-[18px]">{quantities[item.id]}</p>
+                  <Link className="pr-2 h-[40px] w-[40px]" onClick={() => handleIncrease(item.id)}><img src={plus_icon}/></Link>
                 </div>
                 </div>
             </div>
@@ -117,4 +143,5 @@ const Cart = () => {
     </div>
   );
 };
+
 export default Cart;
