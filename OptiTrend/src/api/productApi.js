77589d9e4c83fc47fc3926d65d0b0a67b58ddb87
Aakash kumar <76447347+itsAakashz@users.noreply.github.com>
2024-07-15 18:05:49 +0530

    Update productApi.js

--- OptiTrend/src/api/productApi.js
+++ OptiTrend/src/api/productApi.js
@@ -1,7 +1,7 @@
 export const fetchProducts = async () => {
   try {
     const response = await fetch(
-      "https://timbu-get-all-products.reavdev.workers.dev/organization_id=4108723323df49b18157b4adb5631ef0&size=10&Appid=8N2C3J493DKH2J7&Apikey=b654bdaf159749e9a54345e54485e10c20240712132334365162",
+      "https://timbu-get-all-products.reavdev.workers.dev/?organization_id=4108723323df49b18157b4adb5631ef0&Appid=8N2C3J493DKH2J7&Apikey=b654bdaf159749e9a54345e54485e10c20240712132334365162",
     );
     if (!response.ok) {
        throw new Error(`Failed to fetch products: ${response.status} (${response.statusText})`);
